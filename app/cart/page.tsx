1906055436bb1f294442d361660e29b330bd0a36
Sankar <sankarcule@gmail.com>
2024-03-08 19:58:13 +0530

    Add: Cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Cart() {
+    return (
+        <>
+            <div className="flex m-5 w-full justify-between">
+                <div className="w-3/5 m-5">
+                    <p className="mb-3 font-semibold text-2xl">Order</p>
+                    <div className="border-box rounded min-h-64 bg-white p-6 w-full">
+                        <div className="flex text-black	text-base w-full">
+                            <div className="flex flex-row gap-px justify-start items-start mt-0.5 ml-1 w-3/5">
+                                <Image 
+                                    width={60}
+                                    height={60}
+                                    alt="Picture of the author"
+                                    src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D"/> 
+                                <div className="w-5/6 pl-5 flex flex-col self-center">
+                                    <p className="text-sm font-medium ">Nike Air shoe</p>
+                                    <p className="text-xs font-medium"><span className="text-xs font-normal text-slate-400">Rating:</span> 4.68</p>
+                                </div>
+
+                            </div>
+                            <div className="w-1/5 flex items-center">
+                                <p className="text-base	font-medium	text-lime-400">$1.01</p>
+                                <p className="text-base	font-medium	text-inherit line-through ml-3.5">$1.11</p>
+                            </div>
+                            <div className="w-1/5 flex items-center">
+                                <p className="text-xs font-normal text-slate-400 mr-3.5	">Quantity: 1</p>
+                                <button className="bg-white hover:bg-gray-100 text-gray-800 py-0.5 px-1 border border-gray-400 rounded shadow mr-2">
+                                    -
+                                </button>
+                                <button className="bg-white hover:bg-gray-100 text-gray-800 py-0.5 px-1 border border-gray-400 rounded shadow">
+                                    +
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-2/5 m-5">
+                    <p className="mb-3 font-semibold text-2xl">Payment Summary</p>
+                    <div className="border-box rounded min-h-64 text-black bg-white p-4 w-full">
+                        <div className="flex justify-between my-3">
+                            <p className="text-sm text-slate-500"> Order Summary </p>
+                            <p className="font-medium tracking-widest text-sm"> 1$ </p>
+                        </div>
+                        <div className="flex justify-between my-3">
+                            <p className="text-sm text-slate-500">Additional Service</p>
+                            <p className="font-medium tracking-widest text-sm"> 0.1$ </p>
+                            </div>
+                        <div className="flex justify-between my-3">
+                            <p className="text-sm text-slate-500">Total Amount</p>
+                            <p className="font-medium tracking-widest text-sm"> 0.1$ </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 import React from "react";
 
@@ -11,21 +12,29 @@ export default function Nav() {
             <nav className="p-6 lg:px-8 h-20 border border-t-0 border-l-0 border-r-0 border-b-gray-600">
                 <div className="flex justify-between lg:flex-1">
                     <a href="/" className="-m-1.5 p-1.5">Ecommerce</a>
-                    {
-                            isLoaded && !user &&
-                            <>
+
+                    <div className="flex justify-end items-center">
+                        <a href="/cart" className="flex p-2 mr-4 relative bg-slate-100 rounded-full ">
+                            <Image src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+                            width={20}
+                            height={20}
+                            alt="cart icon"/>
+                            <p className="text-sm text-blue-500	font-bold -mt-4 pl-4 absolute">3</p>
+                        </a>
+                        {
+                                isLoaded && !user &&
                                 <a href="/login" className="-m-1.5 p-1.5">Sign in</a>
-                            </>
-                    }
-                        
-                    {
-                            isLoaded && user &&
-                            <div className="flex w-1/5 justify-end items-center">
-                                <p className="w-1/5"> {user.firstName} </p>
-                                <UserButton afterSignOutUrl="/" />
-                            </div> 
-                    } 
+                        }
+                            
+                        {
+                                isLoaded && user &&
+                                <div className="flex w-1/5 justify-end items-center">
+                                    <p className="w-1/5"> {user.firstName} </p>
+                                    <UserButton afterSignOutUrl="/" />
+                                </div> 
+                        }                    
                 </div> 
+            </div>
             </nav>
         </header>
     )
